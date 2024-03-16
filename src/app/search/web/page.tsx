085c8d4d37374d724b212ea1bd7f679eafb189c7
Ray <rayc12079@gmail.com>
2024-03-16 10:29:35 -0400

    Added fetching data to the google search api

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
-export default function WebSearchPage() {
+type SearchResultType = {
+  title : string,
+  link : string
+  snippet : string,
+}
+
+type Params = {
+  searchParams : {
+    searchTerm : string
+  }
+}
+
+export default async function WebSearchPage(params : Params) {
+  const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.CUSTOM_SEARCH_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${params.searchParams.searchTerm}`)
+  const data = await res.json()
+  const results : SearchResultType[] = data.items
   return (
-    <div>Web Search Page</div>
+    <div>
+      {results && results.map(result => (
+        <Link href={result.link} key={result.title}>{result.title}</Link>
+      ))}
+    </div>
   )
 }
