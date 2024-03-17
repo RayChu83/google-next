d5ad3b5a5fd8c8b374eaaf5f6eaaa283d6da07de
Ray <rayc12079@gmail.com>
2024-03-17 00:05:49 -0400

    Handled Possible Errors while searching

---
+++ src/app/search/error.tsx
@@ -0,0 +1,19 @@
+"use client"
+import React, { useEffect } from 'react'
+
+type Props = {
+    error: Error & { digest?: string };
+    reset: VoidFunction;
+};
+
+export default function Error({error, reset} : Props) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+  return (
+    <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>Something Went Wrong!</h1>
+        <button className='text-blue-500'>Try again</button>
+    </div>
+  )
+}
--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -16,8 +16,24 @@ type Params = {
 
 export default async function WebSearchPage(params : Params) {
   const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.CUSTOM_SEARCH_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${params.searchParams.searchTerm}`)
+  if (!res.ok) throw new Error("Something went wrong")
   const data = await res.json()
   const results : SearchResultType[] = data.items
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for "{params.searchParams.searchTerm}"
+        </h1>
+        <p className='text-lg'>
+          Try searching the web or images for something else
+        </p>
+        <Link href="/" className='text-blue-500'>Go home</Link>
+      </div>
+    )
+  }
+
   return (
     <div>
       {results && results.map(result => (
